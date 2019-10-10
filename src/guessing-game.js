d7f4a2a7cb44f204ce9fbf8c61c17872d1c195be
Asmodei2017 <p.zabavskiy@tut.by>
2019-10-10 22:33:07 +0200

    feat: add task guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.number;
+    }
 
     setRange(min, max) {
+        this.minValue = min;
+        this.maxValue = max;
 
     }
 
     guess() {
+        return this.number = Math.ceil((this.maxValue + this.minValue) / 2);
 
     }
 
     lower() {
-
+        this.maxValue = this.number;
     }
 
     greater() {
-
+        this.minValue = this.number;
     }
 }
 
